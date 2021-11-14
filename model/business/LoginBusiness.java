8568dac56c9485328818608bf4997398b525277c
diannel231 <36435087+diannel231@users.noreply.github.com>
2021-11-14 00:04:52 -0800

    Add files via upload

--- model/business/LoginBusiness.java
+++ model/business/LoginBusiness.java
@@ -7,6 +7,9 @@ import model.entities.User;
 
 public class LoginBusiness {
 	
+	private static User user;
+	private static LoginDataAccess loginDataInstance;
+	
 	public void ValidateAndAuthenticate(String userName, String password) throws ClassNotFoundException, SQLException {
 		
 		if (userName.equals("")) {
@@ -15,10 +18,45 @@ public class LoginBusiness {
 			throw new MessageException("Password not informed.");
 		}
 		
-		User user = new User(userName, password);
+		//User user = new User(userName, password);
+		
+		if(getUserInstance() == null) {
+			
+			user = new User(userName, password);
+		}
+		
+		else {
+			
+			user = getUserInstance();
+			user.setUserName(userName);
+			user.setPassword(password);
+		}
+		
+		if(getUserInstance() == null) {
+			
+			user = new User(userName, password);
+		}
+		
+		else {
+			
+			user = getUserInstance();
+			user.setUserName(userName);
+			user.setPassword(password);
+		}
+		
 		
 		if (!(new LoginDataAccess().verifyCredentials(user))) {
 			throw new MessageException("Incorrect credentials.");
 		}
+	}
+	
+	public static User getUserInstance() {
+		
+		return user;
+	}
+	
+	public static LoginDataAccess getLoginDataInstance() {
+		
+		return loginDataInstance;
 	}
 }
